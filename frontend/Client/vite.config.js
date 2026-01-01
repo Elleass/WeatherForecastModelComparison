39f29817614ee69d0bebe0cfde2a3d60a884498d
Elleass <mic.kalinowski@protonmail.com>
2026-01-01 19:51:58 +0100

    add weather models to db

--- frontend/Client/vite.config.js
+++ frontend/Client/vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(),
-      svgr()
+  plugins: [
+    react(),
+    svgr()
   ],
   server: {
     proxy: {
-      '/api': {
-        // target: 'http://localhost:5189',
-        target: 'http://192.168.0.184:5000',
-        changeOrigin: true,
-        secure: false,
-        // remove rewrite so /api stays intact when forwarded
-        // rewrite: undefined
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true
       }
     }
   }
-})
+});
